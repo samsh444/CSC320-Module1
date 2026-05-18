e97cde0448f37c23424f3d99b6693db9a3c548a2
samsh444 <shawsa65@gmail.com>
2026-05-17 22:03:53 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+package module1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("First Name: Samuel");
+        System.out.println("Last Name: Shaw");
+        System.out.println("Street Address: 444 The Main Street");
+        System.out.println("City: Denver");
+        System.out.println("Zip Code: 80111");
+
+	}
+
+}
+
+
